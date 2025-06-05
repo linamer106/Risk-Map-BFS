9bd07a4a5f26d651211980ea61ac2af379b82d08
Lina Amer <lina088@aucklanduni.ac.nz>
2025-06-05 21:30:18 +1200

    implemented most of the loadMap() still not passing any tests

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -23,28 +23,28 @@ public class MapEngine {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
 
-    // for (String line : countries) {
-    //   String[] parts = line.split(",");
-    //   String name = parts[0].trim();
-    //   String continent = parts[1].trim();
-    //   int fuelCost = Integer.parseInt(parts[2].trim());
-    //   Country country = new Country(name, continent, fuelCost);
-    //   countriesMap.put(name, country); // key: country name
-    // }
+    for (String line : countries) {
+      String[] parts = line.split(",");
+      String name = parts[0].trim();
+      String continent = parts[1].trim();
+      int fuelCost = Integer.parseInt(parts[2].trim());
+      Country country = new Country(name, continent, fuelCost);
+      countriesMap.put(name, country); // key: country name
+    }
 
-    // for (String line : adjacencies) {
-    //   String[] parts = line.split(",");
-    //   String countryName = parts[0].trim();
-    //   Country country = countriesMap.get(countryName);
+    for (String line : adjacencies) {
+      String[] parts = line.split(",");
+      String countryName = parts[0].trim();
+      Country country = countriesMap.get(countryName);
 
-    //   for (int i = 1; i < parts.length; i++) {
-    //     String neighborName = parts[i].trim();
-    //     Country neighbor = countriesMap.get(neighborName);
-    //     if (neighbor != null && !country.getNeighbours().contains(neighbor)) {
-    //       country.addNeighbor(neighbor);
-    //     }
-    //   }
-    // }
+      for (int i = 1; i < parts.length; i++) {
+        String neighborName = parts[i].trim();
+        Country neighbor = countriesMap.get(neighborName);
+        if (neighbor != null && !country.getNeighbours().contains(neighbor)) {
+          country.addNeighbor(neighbor);
+        }
+      }
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
